e94bd0823bb764ce4df32cee048c1d850408a4dd
TinoMadrid <tinomadrid77@gmail.com>
2021-03-30 20:36:01 -0500

    Fixed insert module and cleaned up objects to be correct type.

--- Algorithm/implementation.py
+++ Algorithm/implementation.py
@@ -1,19 +1,22 @@
 from Algorithm import PackageModel
 
+count = 0  # keeps track of the entries and uses it as key
 
-class implementation(PackageModel):
+
+class implementation(PackageModel.Package):
     def __init__(self):
-        self.size = 10000                               # set the initial size of the table
-        self.table = [None] * self.size                 # then initialize the table itself
-        for i in range(self.size):                      # initialize each cell within the table
-            self.table.append([])
+        self.size = 100                                 #set the initial size of the table
+        self.table = []                                 #then initialize the table itself
+        for i in range(self.size):                      #initialize each cell within the table
+            self.table.append([])                       #add lists to each cell within the table
 
     def hashFunction(self, item, tableSize):            #takes the item, gets the integer value and
         return hash(item) % tableSize                   #finds the index of where it ought to be
 
-    def insert(self, item, size):
-        index = self.hashFunction(item, size)           #get the index of where the item will be placed
-        positionList = self.table[index]                #get the linked list of the index
+    def insert(self, item, numberOfItems):
+        index = self.hashFunction(item, 100)            #get the index of where the item will be placed
+        positionList = self.table[index]                #get the list of the index
 
-        keyValue = [index, item]                        #create the new entry
+        keyValue = [numberOfItems, item]                #create the new entry
+        numberOfItems += 1                              #add count to the tracker
         positionList.append(keyValue)                   #now add it to the end of the list
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 ##############################################################################
 ##############################################################################
 #Florentino Madrid 001228177
+#(self, packageID, address, city, state, zip, deliveryDeadline, wgt, specialNotes):
 ##############################################################################
 ##############################################################################
 from IO import readInput
 from Algorithm import implementation
+from Algorithm import PackageModel
 
 if __name__ == '__main__':
-    readInput.read()
-    testImplement = implementation()
+    #readInput.read()
+    #testImplement = implementation(1,"195 W Oakland Ave","Salt Lake City","UT",84115,"###",21, " ")
+    count = 0
+    testImplement = implementation.implementation()
+    testImplement.insert("test", count)
+    print(testImplement.table)
 
