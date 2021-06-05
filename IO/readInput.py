9c3fa342eead7a0724db81bc6295a4cf72c7d773
TinoMadrid <tinomadrid77@gmail.com>
2021-06-05 13:52:57 -0500

    Changed truck loads to adhere to requirements.

--- IO/readInput.py
+++ IO/readInput.py
@@ -7,40 +7,49 @@ def readFile(packageTable):
     newPck3 = Package(3, "233 Canyon Rd", "Salt Lake City", "UT", 84103, "EOD", 2, "Can only be on truck 2", None)
     newPck4 = Package(4, "380 W 2880 S", "Salt Lake City", "UT", 84115, "EOD", 4, None, None)
     newPck5 = Package(5, "410 S State St", "Salt Lake City", "UT", 84111, "EOD", 5, None, None)
-    newPck6 = Package(6, "3060 Lester St", "West Valley City", "UT", 84119, "###", 88, "Delayed on flight---will not arrive to depot until 9:05 am", None)
+    newPck6 = Package(6, "3060 Lester St", "West Valley City", "UT", 84119, "###", 88,
+                      "Delayed on flight---will not arrive to depot until 9:05 am", None)
     newPck7 = Package(7, "1330 2100 S", "Salt Lake City", "UT", 84106, "EOD", 8, None, None)
     newPck8 = Package(8, "300 State St", "Salt Lake City", "UT", 84103, "EOD", 9, None, None)
     newPck9 = Package(9, "300 State St", "Salt Lake City", "UT", 84103, "EOD", 2, "Wrong address listed", None)
     newPck10 = Package(10, "600 E 900", "Salt Lake City", "UT", 84105, "EOD", 1, None, None)
 
     newPck11 = Package(11, "2600 Taylorsville Blvd", "Salt Lake City", "UT", 84118, "EOD", 1, None, None)
-    newPck12 = Package(12, "3575 W Valley Central Station bus Loop", "West Valley City", "UT", 84119, "EOD", 1, None, None)
+    newPck12 = Package(12, "3575 W Valley Central Station bus Loop", "West Valley City", "UT", 84119, "EOD", 1, None,
+                       None)
     newPck13 = Package(13, "2010 W 500 S", "Salt Lake City", "UT", 84104, "###", 2, None, None)
-    newPck14 = Package(14, "4300 S 1300 E", "Millcreek", "UT", 84117, "###", 88, "Must be delivered with 15 and 19", None)
+    newPck14 = Package(14, "4300 S 1300 E", "Millcreek", "UT", 84117, "###", 88, "Must be delivered with 15 and 19",
+                       None)
     newPck15 = Package(15, "4580 S 2300 E", "Holladay", "UT", 84117, "9:00 AM", 4, None, None)
-    newPck16 = Package(16, "4580 S 2300 E", "Holladay", "UT", 84117, "10:30 AM", 88, "Must be delivered with 13 and 19", None)
+    newPck16 = Package(16, "4580 S 2300 E", "Holladay", "UT", 84117, "10:30 AM", 88, "Must be delivered with 13 and 19",
+                       None)
     newPck17 = Package(17, "3148 S 1100 W", "Salt Lake City", "UT", 84119, "EOD", 2, None, None)
     newPck18 = Package(18, "1488 4800 S", "Salt Lake City", "UT", 84123, "EOD", 6, "Can only be on truck 2", None)
     newPck19 = Package(19, "177 W Price Ave", "Salt Lake City", "UT", 84115, "EOD", 37, None, None)
-    newPck20 = Package(20, "3595 Main St", "Salt Lake City", "UT", 84115, "###", 37, "Must be delivered with 13 and 15", None)
+    newPck20 = Package(20, "3595 Main St", "Salt Lake City", "UT", 84115, "###", 37, "Must be delivered with 13 and 15",
+                       None)
 
     newPck21 = Package(21, "3595 Main St", "Salt Lake City", "UT", 84115, "EOD", 3, None, None)
     newPck22 = Package(22, "6351 South 900 East", "Murray", "UT", 84121, "EOD", 2, None, None)
     newPck23 = Package(23, "5100 South 2700 West", "Salt Lake City", "UT", 84118, "EOD", 5, None, None)
     newPck24 = Package(24, "5025 State St", "Murray", "UT", 84107, "EOD", 7, None, None)
-    newPck25 = Package(25, "5383 South 900 East #104", "Salt Lake City", "UT", 84117, "###", 7, "Delayed on flight---will not arrive to depot until 9:05 am", None)
+    newPck25 = Package(25, "5383 South 900 East #104", "Salt Lake City", "UT", 84117, "###", 7,
+                       "Delayed on flight---will not arrive to depot until 9:05 am", None)
     newPck26 = Package(26, "5383 South 900 East #104", "Salt Lake City", "UT", 84117, "EOD", 25, None, None)
     newPck27 = Package(27, "1060 Dalton Ave S", "Salt Lake City", "UT", 84104, "EOD", 5, None, None)
-    newPck28 = Package(28, "2835 Main St", "Salt Lake City", "UT", 84115, "EOD", 7, "Delayed on flight---will not arrive to depot until 9:05 am", None)
+    newPck28 = Package(28, "2835 Main St", "Salt Lake City", "UT", 84115, "EOD", 7,
+                       "Delayed on flight---will not arrive to depot until 9:05 am", None)
     newPck29 = Package(29, "1330 2100 S", "Salt Lake City", "UT", 84106, "###", 2, None, None)
     newPck30 = Package(30, "300 State St", "Salt Lake City", "UT", 84103, "###", 1, None, None)
 
     newPck31 = Package(31, "3365 S 900 W", "Salt Lake City", "UT", 84119, "###", 1, None, None)
-    newPck32 = Package(32, "3365 S 900 W", "Salt Lake City", "UT", 84119, "EOD", 1, "Delayed on flight---will not arrive to depot until 9:05 am", None)
+    newPck32 = Package(32, "3365 S 900 W", "Salt Lake City", "UT", 84119, "EOD", 1,
+                       "Delayed on flight---will not arrive to depot until 9:05 am", None)
     newPck33 = Package(33, "2530 S 500 E", "Salt Lake City", "UT", 84106, "EOD", 1, None, None)
     newPck34 = Package(34, "4580 S 2300 E", "Holladay", "UT", 84117, "###", 2, None, None)
     newPck35 = Package(35, "1060 Dalton Ave S", "Salt Lake City", "UT", 84104, "EOD", 88, None, None)
-    newPck36 = Package(36, "2300 Parkway Blvd", "West Valley City", "UT", 84119, "EOD", 88, "Can only be on truck 2", None)
+    newPck36 = Package(36, "2300 Parkway Blvd", "West Valley City", "UT", 84119, "EOD", 88, "Can only be on truck 2",
+                       None)
     newPck37 = Package(37, "410 S State St", "Salt Lake City", "UT", 84111, "###", 2, None, None)
     newPck38 = Package(38, "410 S State St", "Salt Lake City", "UT", 84111, "EOD", 9, "Can only be on truck 2", None)
     newPck39 = Package(39, "2010 W 500 S", "Salt Lake City", "UT", 84104, "EOD", 9, None, None)
@@ -154,31 +163,62 @@ def readFile(packageTable):
     distanceTable[1].append(4.4)
     distanceTable[1].append(13.0)
 
-    distanceTable[2].extend((3.8, 7.1, 0.0, 9.2, 4.4, 2.8, 8.6, 6.3, 5.3, 1.6, 10.4, 3.0, 5.3, 6.5, 5.6, 5.8, 5.7, 4.1, 3.6, 4.3, 3.3, 5.0, 6.1, 9.7, 6.1, 2.8, 7.4))
-    distanceTable[3].extend((11.0, 6.4, 9.2, 0.0, 5.6, 6.9, 8.6, 4.0, 11.1, 7.3, 1.0, 6.4, 11.1, 3.9, 4.3, 4.4, 7.2, 5.3, 6.0, 10.6, 5.9, 7.4, 4.7, 0.6, 6.4, 10.1, 10.1))
-    distanceTable[4].extend((2.2, 6.0, 4.4, 5.6, 0.0, 1.9, 7.9, 5.1, 7.5, 2.6, 6.5, 1.5, 7.5, 3.2, 2.4, 2.7, 1.4, 0.5, 1.7, 6.5, 3.2, 5.2, 2.5, 6.0, 4.2, 5.4, 5.5))
-    distanceTable[5].extend((3.5, 4.8, 2.8, 6.9, 1.9, 0.0, 6.3, 4.3, 4.5, 1.5, 8.7, 0.8, 4.5, 3.9, 3.0, 3.8, 5.7, 1.9, 1.1, 3.5, 4.9, 6.9, 4.2, 9.0, 5.9, 3.5, 7.2))
-    distanceTable[6].extend((10.9, 1.6, 8.6, 8.6, 7.9, 6.3, 0.0, 4.0, 4.2, 8.0, 8.6, 6.9, 4.2, 4.2, 8.0, 5.8, 7.2, 7.7, 6.6, 3.2, 11.2, 12.7, 10.0, 8.2, 11.7, 5.1, 14.2))
-    distanceTable[7].extend((8.6, 2.8, 6.3, 4.0, 5.1, 4.3, 4.0, 0.0, 7.7, 9.3, 4.6, 4.8, 7.7, 1.6, 3.3, 3.4, 3.1, 5.1, 4.6, 6.7, 8.1, 10.4, 7.8, 4.2, 9.5, 6.2, 10.7))
-    distanceTable[8].extend((7.6, 4.8, 5.3, 11.1, 7.5, 4.5, 4.2, 7.7, 0.0, 4.8, 11.9, 4.7, 0.6, 7.6, 7.8, 6.6, 7.2, 5.9, 5.4, 1.0, 8.5, 10.3, 7.8, 11.5, 9.5, 2.8, 14.1))
-    distanceTable[9].extend((2.8, 6.3, 1.6, 7.3, 2.6, 1.5, 8.0, 9.3, 4.8, 0.0, 9.4, 1.1, 5.1, 4.6, 3.7, 4.0, 6.7, 2.3, 1.8, 4.1, 3.8, 5.8, 4.3, 7.8, 4.8, 3.2, 6.0))
-    distanceTable[10].extend((6.4, 7.3, 10.4, 1.0, 6.5, 8.7, 8.6, 4.6, 11.9, 9.4, 0.0, 7.3, 12.0, 4.9, 5.2, 5.4, 8.1, 6.2, 6.9, 11.5, 6.9, 8.3, 4.1, 0.4, 4.9, 11.0, 6.8))
-    distanceTable[11].extend((3.2, 5.3, 3.0, 6.4, 1.5, 0.8, 6.9, 4.8, 4.7, 1.1, 7.3, 0.0, 4.7, 3.5, 2.6, 2.9, 6.3, 1.2, 1.0, 3.7, 4.1, 6.2, 3.4, 6.9, 5.2, 3.7, 6.4))
-    distanceTable[12].extend((7.6, 4.8, 5.3, 11.1, 7.5, 4.5, 4.2, 7.7, 0.6, 5.1, 12.0, 4.7, 0.0, 7.3, 7.8, 6.6, 7.2, 5.9, 5.4, 1.0, 8.5, 10.3, 7.8, 11.5, 9.5, 2.8, 14.1))
-    distanceTable[13].extend((5.2, 3.0, 6.5, 3.9, 3.2, 3.9, 4.2, 1.6, 7.6, 4.6, 4.9, 3.5, 7.3, 0.0, 1.3, 1.5, 4.0, 3.2, 3.0, 6.9, 6.2, 8.2, 5.5, 4.4, 7.2, 6.4, 10.5))
-    distanceTable[14].extend((4.4, 4.6, 5.6, 4.3, 2.4, 3.0, 8.0, 3.3, 7.8, 3.7, 5.2, 2.6, 7.8, 1.3, 0.0, 0.6, 6.4, 2.4, 2.2, 6.8, 5.3, 7.4, 4.6, 4.8, 6.3, 6.5, 8.8))
-    distanceTable[15].extend((3.7, 4.5, 5.8, 4.4, 2.7, 3.8, 5.8, 3.4, 6.6, 4.0, 5.4, 2.9, 6.6, 1.5, 0.6, 0.0, 5.6, 1.6, 1.7, 6.4, 4.9, 6.9, 4.2, 5.6, 5.9, 5.7, 8.4))
-    distanceTable[16].extend((7.6, 7.4, 5.7, 7.2, 1.4, 5.7, 7.2, 3.1, 7.2, 6.7, 8.1, 6.3, 7.2, 4.0, 6.4, 5.6, 0.0, 7.1, 6.1, 7.2, 10.6, 12.0, 9.4, 7.5, 11.1, 6.2, 13.6))
-    distanceTable[17].extend((2.0, 6.0, 4.1, 5.3, 0.5, 1.9, 7.7, 5.1, 5.9, 2.3, 6.2, 1.2, 5.9, 3.2, 2.4, 1.6, 7.1, 0.0, 1.6, 4.9, 3.0, 5.0, 2.3, 5.5, 4.0, 5.1, 5.2))
-    distanceTable[18].extend((3.6, 5.0, 3.6, 6.0, 1.7, 1.1, 6.6, 4.6, 5.4, 1.8, 6.9, 1.0, 5.4, 3.0, 2.2, 1.7, 6.1, 1.6, 0.0, 4.4, 4.6, 6.6, 3.9, 6.5, 5.6, 4.3, 6.9))
-    distanceTable[19].extend((6.5, 4.8, 4.3, 10.6, 6.5, 3.5, 3.2, 6.7, 1.0, 4.1, 11.5, 3.7, 1.0, 6.9, 6.8, 6.4, 7.2, 4.9, 4.4, 0.0, 7.5, 9.3, 6.8, 11.4, 8.5, 1.8, 13.1))
-    distanceTable[20].extend((1.9, 9.5, 3.3, 5.9, 3.2, 4.9, 11.2, 8.1, 8.5, 3.8, 6.9, 4.1, 8.5, 6.2, 5.3, 4.9, 10.6, 3.0, 4.6, 7.5, 0.0, 2.0, 2.9, 6.4, 2.8, 6.0, 4.1))
-    distanceTable[21].extend((3.4, 10.9, 5.0, 7.4, 5.2, 6.9, 12.7, 10.4, 10.3, 5.8, 8.3, 6.2, 10.3, 8.2, 7.4, 6.9, 12.0, 5.0, 6.6, 9.3, 2.0, 0.0, 4.4, 7.9, 3.4, 7.9, 4.7))
-    distanceTable[22].extend((2.4, 8.3, 6.1, 4.7, 2.5, 4.2, 10.0, 7.8, 7.8, 4.3, 4.1, 3.4, 7.8, 5.5, 4.6, 4.2, 9.4, 2.3, 3.9, 6.8, 2.9, 4.4, 0.0, 4.5, 1.7, 6.8, 3.1))
-    distanceTable[23].extend((6.4, 6.9, 9.7, 0.6, 6.0, 9.0, 8.2, 4.2, 11.5, 7.8, 0.4, 6.9, 11.5, 4.4, 4.8, 5.6, 7.5, 5.5, 6.5, 11.4, 6.4, 7.9, 4.5, 0.0, 5.4, 10.6, 7.8))
-    distanceTable[24].extend((2.4, 10.0, 6.1, 6.4, 4.2, 5.9, 11.7, 9.5, 9.5, 4.8, 4.9, 5.2, 9.5, 7.2, 6.3, 5.9, 11.1, 4.0, 5.6, 8.5, 2.8, 3.4, 1.7, 5.4, 0.0, 7.0, 1.3))
-    distanceTable[25].extend((5.0, 4.4, 2.8, 10.1, 5.4, 3.5, 5.1, 6.2, 2.8, 3.2, 11.0, 3.7, 2.8, 6.4, 6.5, 5.7, 6.2, 5.1, 4.3, 1.8, 6.0, 7.9, 6.8, 10.6, 7.0, 0.0, 8.3))
-    distanceTable[26].extend((3.6, 13.0, 7.4, 10.1, 5.5, 7.2, 14.2, 10.7, 14.1, 6.0, 6.8, 6.4, 14.1, 10.5, 8.8, 8.4, 13.6, 5.2, 6.9, 13.1, 4.1, 4.7, 3.1, 7.8, 1.3, 8.3, 0.0))
+    distanceTable[2].extend((3.8, 7.1, 0.0, 9.2, 4.4, 2.8, 8.6, 6.3, 5.3, 1.6, 10.4, 3.0, 5.3, 6.5, 5.6, 5.8, 5.7, 4.1,
+                             3.6, 4.3, 3.3, 5.0, 6.1, 9.7, 6.1, 2.8, 7.4))
+    distanceTable[3].extend((
+                            11.0, 6.4, 9.2, 0.0, 5.6, 6.9, 8.6, 4.0, 11.1, 7.3, 1.0, 6.4, 11.1, 3.9, 4.3, 4.4, 7.2, 5.3,
+                            6.0, 10.6, 5.9, 7.4, 4.7, 0.6, 6.4, 10.1, 10.1))
+    distanceTable[4].extend((2.2, 6.0, 4.4, 5.6, 0.0, 1.9, 7.9, 5.1, 7.5, 2.6, 6.5, 1.5, 7.5, 3.2, 2.4, 2.7, 1.4, 0.5,
+                             1.7, 6.5, 3.2, 5.2, 2.5, 6.0, 4.2, 5.4, 5.5))
+    distanceTable[5].extend((3.5, 4.8, 2.8, 6.9, 1.9, 0.0, 6.3, 4.3, 4.5, 1.5, 8.7, 0.8, 4.5, 3.9, 3.0, 3.8, 5.7, 1.9,
+                             1.1, 3.5, 4.9, 6.9, 4.2, 9.0, 5.9, 3.5, 7.2))
+    distanceTable[6].extend((10.9, 1.6, 8.6, 8.6, 7.9, 6.3, 0.0, 4.0, 4.2, 8.0, 8.6, 6.9, 4.2, 4.2, 8.0, 5.8, 7.2, 7.7,
+                             6.6, 3.2, 11.2, 12.7, 10.0, 8.2, 11.7, 5.1, 14.2))
+    distanceTable[7].extend((8.6, 2.8, 6.3, 4.0, 5.1, 4.3, 4.0, 0.0, 7.7, 9.3, 4.6, 4.8, 7.7, 1.6, 3.3, 3.4, 3.1, 5.1,
+                             4.6, 6.7, 8.1, 10.4, 7.8, 4.2, 9.5, 6.2, 10.7))
+    distanceTable[8].extend((7.6, 4.8, 5.3, 11.1, 7.5, 4.5, 4.2, 7.7, 0.0, 4.8, 11.9, 4.7, 0.6, 7.6, 7.8, 6.6, 7.2, 5.9,
+                             5.4, 1.0, 8.5, 10.3, 7.8, 11.5, 9.5, 2.8, 14.1))
+    distanceTable[9].extend((2.8, 6.3, 1.6, 7.3, 2.6, 1.5, 8.0, 9.3, 4.8, 0.0, 9.4, 1.1, 5.1, 4.6, 3.7, 4.0, 6.7, 2.3,
+                             1.8, 4.1, 3.8, 5.8, 4.3, 7.8, 4.8, 3.2, 6.0))
+    distanceTable[10].extend((6.4, 7.3, 10.4, 1.0, 6.5, 8.7, 8.6, 4.6, 11.9, 9.4, 0.0, 7.3, 12.0, 4.9, 5.2, 5.4, 8.1,
+                              6.2, 6.9, 11.5, 6.9, 8.3, 4.1, 0.4, 4.9, 11.0, 6.8))
+    distanceTable[11].extend((3.2, 5.3, 3.0, 6.4, 1.5, 0.8, 6.9, 4.8, 4.7, 1.1, 7.3, 0.0, 4.7, 3.5, 2.6, 2.9, 6.3, 1.2,
+                              1.0, 3.7, 4.1, 6.2, 3.4, 6.9, 5.2, 3.7, 6.4))
+    distanceTable[12].extend((
+                             7.6, 4.8, 5.3, 11.1, 7.5, 4.5, 4.2, 7.7, 0.6, 5.1, 12.0, 4.7, 0.0, 7.3, 7.8, 6.6, 7.2, 5.9,
+                             5.4, 1.0, 8.5, 10.3, 7.8, 11.5, 9.5, 2.8, 14.1))
+    distanceTable[13].extend((5.2, 3.0, 6.5, 3.9, 3.2, 3.9, 4.2, 1.6, 7.6, 4.6, 4.9, 3.5, 7.3, 0.0, 1.3, 1.5, 4.0, 3.2,
+                              3.0, 6.9, 6.2, 8.2, 5.5, 4.4, 7.2, 6.4, 10.5))
+    distanceTable[14].extend((4.4, 4.6, 5.6, 4.3, 2.4, 3.0, 8.0, 3.3, 7.8, 3.7, 5.2, 2.6, 7.8, 1.3, 0.0, 0.6, 6.4, 2.4,
+                              2.2, 6.8, 5.3, 7.4, 4.6, 4.8, 6.3, 6.5, 8.8))
+    distanceTable[15].extend((3.7, 4.5, 5.8, 4.4, 2.7, 3.8, 5.8, 3.4, 6.6, 4.0, 5.4, 2.9, 6.6, 1.5, 0.6, 0.0, 5.6, 1.6,
+                              1.7, 6.4, 4.9, 6.9, 4.2, 5.6, 5.9, 5.7, 8.4))
+    distanceTable[16].extend((7.6, 7.4, 5.7, 7.2, 1.4, 5.7, 7.2, 3.1, 7.2, 6.7, 8.1, 6.3, 7.2, 4.0, 6.4, 5.6, 0.0, 7.1,
+                              6.1, 7.2, 10.6, 12.0, 9.4, 7.5, 11.1, 6.2, 13.6))
+    distanceTable[17].extend((2.0, 6.0, 4.1, 5.3, 0.5, 1.9, 7.7, 5.1, 5.9, 2.3, 6.2, 1.2, 5.9, 3.2, 2.4, 1.6, 7.1, 0.0,
+                              1.6, 4.9, 3.0, 5.0, 2.3, 5.5, 4.0, 5.1, 5.2))
+    distanceTable[18].extend((3.6, 5.0, 3.6, 6.0, 1.7, 1.1, 6.6, 4.6, 5.4, 1.8, 6.9, 1.0, 5.4, 3.0, 2.2, 1.7, 6.1, 1.6,
+                              0.0, 4.4, 4.6, 6.6, 3.9, 6.5, 5.6, 4.3, 6.9))
+    distanceTable[19].extend((
+                             6.5, 4.8, 4.3, 10.6, 6.5, 3.5, 3.2, 6.7, 1.0, 4.1, 11.5, 3.7, 1.0, 6.9, 6.8, 6.4, 7.2, 4.9,
+                             4.4, 0.0, 7.5, 9.3, 6.8, 11.4, 8.5, 1.8, 13.1))
+    distanceTable[20].extend((
+                             1.9, 9.5, 3.3, 5.9, 3.2, 4.9, 11.2, 8.1, 8.5, 3.8, 6.9, 4.1, 8.5, 6.2, 5.3, 4.9, 10.6, 3.0,
+                             4.6, 7.5, 0.0, 2.0, 2.9, 6.4, 2.8, 6.0, 4.1))
+    distanceTable[21].extend((3.4, 10.9, 5.0, 7.4, 5.2, 6.9, 12.7, 10.4, 10.3, 5.8, 8.3, 6.2, 10.3, 8.2, 7.4, 6.9, 12.0,
+                              5.0, 6.6, 9.3, 2.0, 0.0, 4.4, 7.9, 3.4, 7.9, 4.7))
+    distanceTable[22].extend((2.4, 8.3, 6.1, 4.7, 2.5, 4.2, 10.0, 7.8, 7.8, 4.3, 4.1, 3.4, 7.8, 5.5, 4.6, 4.2, 9.4, 2.3,
+                              3.9, 6.8, 2.9, 4.4, 0.0, 4.5, 1.7, 6.8, 3.1))
+    distanceTable[23].extend((
+                             6.4, 6.9, 9.7, 0.6, 6.0, 9.0, 8.2, 4.2, 11.5, 7.8, 0.4, 6.9, 11.5, 4.4, 4.8, 5.6, 7.5, 5.5,
+                             6.5, 11.4, 6.4, 7.9, 4.5, 0.0, 5.4, 10.6, 7.8))
+    distanceTable[24].extend((2.4, 10.0, 6.1, 6.4, 4.2, 5.9, 11.7, 9.5, 9.5, 4.8, 4.9, 5.2, 9.5, 7.2, 6.3, 5.9, 11.1,
+                              4.0, 5.6, 8.5, 2.8, 3.4, 1.7, 5.4, 0.0, 7.0, 1.3))
+    distanceTable[25].extend((
+                             5.0, 4.4, 2.8, 10.1, 5.4, 3.5, 5.1, 6.2, 2.8, 3.2, 11.0, 3.7, 2.8, 6.4, 6.5, 5.7, 6.2, 5.1,
+                             4.3, 1.8, 6.0, 7.9, 6.8, 10.6, 7.0, 0.0, 8.3))
+    distanceTable[26].extend((3.6, 13.0, 7.4, 10.1, 5.5, 7.2, 14.2, 10.7, 14.1, 6.0, 6.8, 6.4, 14.1, 10.5, 8.8, 8.4,
+                              13.6, 5.2, 6.9, 13.1, 4.1, 4.7, 3.1, 7.8, 1.3, 8.3, 0.0))
     ############################################################################################################
     addressTable[0].extend(("Western Governors University 4001 South 700 East(84107)",
                             " Western Governors University 4001 South 700 East",
@@ -793,13 +833,15 @@ def address_lookup(fromLocation, toLocation, addressTable, distanceTable):
             for x in range(len(begin)):
                 if toLocation in addressTable[tracker][x]:
                     matchingCoordinates.append(tracker)
-                    matchingCoordinates.append(x-1) # subtracting 1 since the first string is the next stop, and not doing so would return the index for the
-                    break                           # from location which anything from and to itself is 0.
+                    matchingCoordinates.append(
+                        x - 1)  # subtracting 1 since the first string is the next stop, and not doing so would return the index for the
+                    break  # from location which anything from and to itself is 0.
             if len(matchingCoordinates) != 0:
                 if matchingCoordinates[1] == 27: matchingCoordinates[1] = matchingCoordinates[1] - 1
                 return distanceTable[matchingCoordinates[0]][matchingCoordinates[1]]
         tracker += 1
-    return address_lookup(fromLocation, toLocation, addressTable, distanceTable) # function calls itself if the from location is located behind tracker count
+    return address_lookup(fromLocation, toLocation, addressTable,
+                          distanceTable)  # function calls itself if the from location is located behind tracker count
 
 
 def loadTrucks(truckOne, truckTwo, packageTable):
@@ -839,8 +881,72 @@ def loadTrucks(truckOne, truckTwo, packageTable):
     resultTruckTwo.remove(39)
     resultTruckTwo.remove(35)
 
-    ##add the beginning addresses of the trucks
+    # NEXT need to change loading to meet requirements for project.
+    ## add the beginning addresses of the trucks
     resultTruckOne.append("Western Governors University 4001 South 700 East(84107)")
     resultTruckTwo.append("Western Governors University 4001 South 700 East(84107)")
 
     return resultTruckOne, resultTruckTwo
+
+
+def truckOneLoadOne(truckOne):
+    truckOne.append(1)
+    truckOne.append(2)
+    truckOne.append(4)
+    truckOne.append(5)
+    truckOne.append(7)
+
+    truckOne.append(8)
+    truckOne.append(10)
+    truckOne.append(11)
+    truckOne.append(12)
+    truckOne.append(13)
+
+    truckOne.append(14)
+    truckOne.append(15)
+    truckOne.append(16)
+    truckOne.append(17)
+    truckOne.append(19)
+
+    truckOne.append(20)
+    return truckOne
+
+
+def truckTwoLoadOne(truckTwo):
+    # truck leaves at 9:05
+    truckTwo.append(3)
+    truckTwo.append(6)
+    truckTwo.append(18)
+    truckTwo.append(21)
+    truckTwo.append(22)
+
+    truckTwo.append(23)
+    truckTwo.append(24)
+    truckTwo.append(25)
+    truckTwo.append(26)
+    truckTwo.append(27)
+
+    truckTwo.append(28)
+    truckTwo.append(29)
+    truckTwo.append(30)
+    truckTwo.append(32)
+    truckTwo.append(36)
+
+    truckTwo.append(38)
+    return truckTwo
+
+
+def truckOneLoadTwo(truckOne):
+    # packages on truck one after 10:32
+
+    truckOne.append(9)
+    truckOne.append(31)
+    truckOne.append(33)
+    truckOne.append(34)
+    truckOne.append(35)
+
+    truckOne.append(37)
+    truckOne.append(39)
+    truckOne.append(40)
+
+    return truckOne
